539083bad748c8e2816295faebf565937a3a4b4d
arav39marri <aravindmarripelli@gmail.com>
2024-09-22 23:09:27 +0530

    cmtd

--- backend/model.js
+++ backend/model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const databaseUrl = process.env.DATABASE_URL;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const curuser = new mongoose.Schema({
     name: { type: String, required: true },
     handle: { type: String, required: true },
